2218702eb9ad3f98f2d33cdc9c2459393473fd65
Sreedhar Chidambaram <sreedhar@capterra.com>
2016-07-11 19:49:40 +0000

    No need for include_recipe

--- recipes/install-mongo.rb
+++ recipes/install-mongo.rb
@@ -1,5 +1,3 @@
-include_recipe 'yum'
-
 yum_repository 'mongodb-org' do
   description 'MongoDB Repo'
   baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
@@ -10,10 +8,6 @@ yum_repository 'mongodb-org' do
 end
 
 package 'mongodb-org'
-#package 'mongodb-org-server'
-#package 'mongodb-org-mongos'
-#package 'mongodb-org-shell'
-#package 'mongodb-org-tools'
 
 service 'mongod' do
   action [ :enable, :start ]
