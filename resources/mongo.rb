3eecacd616531b661b69fa112ac1428b52657aa4
Ron Eickler <ron.eickler@stratalux.com>
2016-07-11 19:57:14 +0000

    Added version property

--- resources/mongo.rb
+++ resources/mongo.rb
@@ -1,13 +1,13 @@
 resource_name :otter_mongod
 
-#property :name, RubyType, default: 'value'
+property :version, String, default: '3.2'
 
 action :install do
   yum_repository 'mongodb-org' do
     description 'MongoDB Repo'
-    baseurl 'https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.2/x86_64/'
+    baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/#{version}/x86_64/"
     gpgcheck true
-    gpgkey 'https://www.mongodb.org/static/pgp/server-3.2.asc'
+    gpgkey "https://www.mongodb.org/static/pgp/server-#{version}.asc"
     enabled true
     action :create
   end
